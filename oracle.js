bfd3783ed6f827aaf49ad5550319b3eb3fccad86
mgsgde <mgsgde@gmail.com>
2017-10-07 22:54:59 +0200

    bug

--- oracle.js
+++ oracle.js
@@ -57,7 +57,7 @@ function requestApi(result) {
         console.log("Requested fraport API. Body:", body)
         if (!error && response && response.statusCode == 200) {
             let info = JSON.parse(body);
-            instanceBadWeatherContract.__callback(id, (parseInt(uppersnow_cm)*100))
+            instanceBadWeatherContract.__callback(id, (parseInt(info.uppersnow_cm)*100))
         }
     }
 
